a38771959e1c61070823fb535da7c07e319f6d01
Gibson Farabow <gibsonfarabow@gmail.com>
2019-03-19 12:25:03 -0400

    Update DemocratQuery.py

--- DemocratQuery.py
+++ DemocratQuery.py
@@ -9,15 +9,18 @@ and then from there tweets are further grouped, in this case by a NC location
 be analyzed. This script finds the average amount of followers that NC twitter users have who
 mention the candidates.
 '''
+
 import csv
 import tweepy
 import time
 import pandas
 
 # enter
-dpath = ' ' # for location of csv file
+dpath = ' ' # for location of csv file to be created
+file = open(dpath + 'Dem_Candidate_Query.csv', 'w') # will be appended later
+file.close()
 
-# enter
+# enter Twitter API information
 consumer_key = ' '
 consumer_secret =  ' '
 access_token = ' '
@@ -25,11 +28,11 @@ access_token_secret = ' '
 
 # Tweepy initialization
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret) 
+auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 
-x = 1000  # amount of searches/items; there is a function to handle Twitter's rate limit  
+x = 1000  # amount of search items; this program has code to handle Twitter's rate limit
 
 def NC_Candidate_Query(candidate):
     all_users = []
@@ -41,7 +44,7 @@ def NC_Candidate_Query(candidate):
             all_users.append(tweets.user)
             tweet_count += 1
             if 'Trump' in tweets.text:
-                trump_count += 1 # see print statement
+                trump_count += 1 # see print statement in output
 
     results = [[candidate, tweet.location, tweet.followers_count] for tweet in all_users]
 
@@ -58,27 +61,25 @@ def NC_Candidate_Query(candidate):
 candidates = ['Biden', 'Bernie', "O'Rourke", 'Booker', 'Klobuchar', ' Elizabeth Warren', \
               'Senator Warren', '@SenWarren','Senator Harris', 'Kamala Harris', '@SenKamalaHarris']
 
-def run_without_limit(candidates):
+def run_without_limit(candidates): # keeps going after data rate limit exceeded
         for candidate in candidates:
             try:
-                NC_Candidate_Query(candidate)   
+                NC_Candidate_Query(candidate)
             except tweepy.TweepError:
                 print('Rate Limit Exceeded: Sleeping')
-                time.sleep(60 * 15) # Rate limit resets after 15 minutes
+                time.sleep(60 * 15) # rate limit resets after 15 minutes
                 # with x = 1000, program can go through two candidates before limit exceeded
                 continue
-        
+
 def average_user_followers(candidate):
         print("")
         data = pandas.read_csv(dpath + 'Dem_Candidate_Query.csv')
         data = data[data.Candidate == candidate]
         data.Followers = pandas.to_numeric(data.Followers, errors='coerce')
-        print('Accounts that mention ' + candidate + ' have this many followers on average: ') 
+        print('Accounts that mention ' + candidate + ' have this many followers on average: ')
         print(data.Followers.mean())
 
-# run script        
+# run script
 run_without_limit(candidates)
 for candidate in candidates:
         average_user_followers(candidate)
-        
-
